5efc8902e014de5f9fce47924560638706b8a29f
Oleksii <mysterious240@gmail.com>
2023-05-11 15:18:35 +0300

    last changes

--- createTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE utc_time_data (
-  original_timestamp TIMESTAMP,
-  original_timezone STRING,
-  timestamp_milliseconds INTEGER,
-  timezone STRING,
-  day INTEGER,
-  month INTEGER,
-  year INTEGER
-);
