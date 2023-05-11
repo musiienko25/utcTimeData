125323993726614ce0515fc7a3379b3a65ca9cf1
Oleksii <mysterious240@gmail.com>
2023-05-11 23:19:57 +0300

    deleted test

--- app.js
+++ app.js
@@ -33,25 +33,23 @@ const insertRowToBigQuery = async (mdtTimestamp) => {
   const timestamp = mdtTimestamp;
   const timezone = "MDT";
   const day = Number(date);
-
-  const row = {
-    originalTimestamp: originalTimestamp,
-    originalTimezone: originalTimezone,
-    timestamp: String(timestamp),
-    timezone: timezone,
-    day: String(day),
-    month: month,
-    year: year,
-  };
-
+  const row = [
+    {
+      string_field_0: originalTimestamp,
+      string_field_1: originalTimezone,
+      string_field_2: String(timestamp),
+      string_field_3: timezone,
+      string_field_4: String(day),
+      string_field_5: month,
+      string_field_6: year,
+    },
+  ];
   console.log(row);
-
   try {
     const [result] = await bigqueryClient
       .dataset(process.env.DATASET_ID)
       .table(process.env.TABLE_ID)
       .insert(row);
-
     console.log(`Inserted row: ${JSON.stringify(result)}`);
   } catch (error) {
     console.error(`Failed to insert row to BigQuery: ${error}`);
@@ -59,25 +57,8 @@ const insertRowToBigQuery = async (mdtTimestamp) => {
 };
 
 const main = async () => {
-  // const { data: utcData } = await getCurrentUTCTime();
-  const test = {
-    abbreviation: "UTC",
-    client_ip: "178.151.207.216",
-    datetime: "2023-05-11T13:31:08.193649+00:00",
-    day_of_week: 4,
-    day_of_year: 131,
-    dst: false,
-    dst_from: null,
-    dst_offset: 0,
-    dst_until: null,
-    raw_offset: 0,
-    timezone: "Etc/UTC",
-    unixtime: 1683811868,
-    utc_datetime: "2023-05-11T13:31:08.193649+00:00",
-    utc_offset: "+00:00",
-    week_number: 19,
-  };
-  const utcData = test;
+  const { data: utcData } = await getCurrentUTCTime();
+
   if (utcData) {
     const mdtTimestamp = convertToMDTTimestamp(utcData);
     insertRowToBigQuery(mdtTimestamp);
